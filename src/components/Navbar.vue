2caa6827546c7c805580e08a31264a593b8dfb5a
Inès <120586445+Michette@users.noreply.github.com>
2024-10-09 14:14:04 +0200

    vue set up and navbar start

---
+++ src/components/Navbar.vue
@@ -0,0 +1,21 @@
+<script setup>
+  import IconFavourite from './icons/IconFavourite.vue';
+  import IconSearch from './icons/IconSearch.vue';
+</script>
+
+<template>
+  <div class="flex justify-between bg-pink-500 py-4">
+    <nav class="flex flex-wrap gap-4 ml-8">
+      <RouterLink to="/">Acceuil</RouterLink>
+      <RouterLink to="/shop">Boutique</RouterLink>
+      <RouterLink to="/custom-nails">Ongles personnalisés</RouterLink>
+      <RouterLink to="/find-your-size">Trouver sa taille</RouterLink>
+      <RouterLink to="/guide">Guide</RouterLink>
+      <RouterLink to="/faq">FAQ</RouterLink>
+    </nav>
+    <div>
+      <button class=""><IconSearch /></button>
+      <button class=""><IconFavourite /></button>
+    </div>
+  </div>
+</template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,28 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Lato', ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        pink: {
+          100: '#fef1f8',
+          200: '#ffe9f5',
+          300: '#fddfef',
+          400: '#ffd7ec',
+          500: '#ffc9e5',
+          600: '#ffb3db',
+          700: '#ffa5d4',
+          800: '#fd83c4',
+          900: '#d1516d',
+        },
+        silver: '#f8f8f8',
+      },
+    },
   },
   plugins: [],
-}
-
+};
